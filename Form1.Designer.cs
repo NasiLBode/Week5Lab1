b22661203b8507dfd7d58b6653715c026c75b2a7
Nasi Bode <nasibode@gmail.com>
2021-09-26 22:01:24 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Week5Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.speedInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.limitInput = new System.Windows.Forms.TextBox();
+            this.resultOutput = new System.Windows.Forms.Label();
+            this.penaltyOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 128);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(93, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Speed (MPH) :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // speedInput
+            // 
+            this.speedInput.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.speedInput.Location = new System.Drawing.Point(452, 44);
+            this.speedInput.Name = "speedInput";
+            this.speedInput.Size = new System.Drawing.Size(126, 50);
+            this.speedInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(93, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Speed Limit (MPH) :";
+            // 
+            // limitInput
+            // 
+            this.limitInput.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.limitInput.Location = new System.Drawing.Point(452, 123);
+            this.limitInput.Name = "limitInput";
+            this.limitInput.Size = new System.Drawing.Size(126, 50);
+            this.limitInput.TabIndex = 4;
+            // 
+            // resultOutput
+            // 
+            this.resultOutput.AutoSize = true;
+            this.resultOutput.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resultOutput.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.resultOutput.Location = new System.Drawing.Point(138, 375);
+            this.resultOutput.Name = "resultOutput";
+            this.resultOutput.Size = new System.Drawing.Size(0, 30);
+            this.resultOutput.TabIndex = 5;
+            this.resultOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // penaltyOutput
+            // 
+            this.penaltyOutput.AutoSize = true;
+            this.penaltyOutput.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.penaltyOutput.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.penaltyOutput.Location = new System.Drawing.Point(517, 375);
+            this.penaltyOutput.Name = "penaltyOutput";
+            this.penaltyOutput.Size = new System.Drawing.Size(0, 30);
+            this.penaltyOutput.TabIndex = 6;
+            this.penaltyOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.penaltyOutput);
+            this.Controls.Add(this.resultOutput);
+            this.Controls.Add(this.limitInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.speedInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Speed Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox speedInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox limitInput;
+        private System.Windows.Forms.Label resultOutput;
+        private System.Windows.Forms.Label penaltyOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿/*File Name: Week5Lab1
+ * Programmer: Nasi Bode
+ * Problem Statement: Create a GUI that takes in a speed limit and your speed. Add a Canvas element that
+ * changes color, green (legal speed), yellow (speeding no penalty), red (speeding with penalty). Add
+ * a label that in addition to the color, prints out the status as well. 
+ * 
+ * Overall Plan: 
+ * 1. Create GUI platform
+ * 2. Create two  labels for user (1 for user speed, 1 for speed limit)
+ * 3. Create two text boxes for user input (1 for user speed, 1 for speed limit)
+ * 4. Creaet Button to start calculation
+ * 5. Create empty label to display result
+ * 6. Convert string input from text box to int
+ * 7. Define the variable for fine and penalty 
+ * 8. Create multi if-else statment
+ * 8. If user is above 25 speed limit - output result to the empty string
+ * 9. If user is above speed limit but under 25 - output result to the empty string
+ * 10. If user is legal - output result to empty string
+ *
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week5Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //initialize variables from user input
+            int speed = int.Parse(speedInput.Text);
+            int limit = int.Parse(limitInput.Text);
+            int fine = 7;
+            int penalty = 250;
+            int result;
+            int difference = speed - limit;
+
+            //create multi if-else statement to determine if user gets a penalty
+            if (speed > limit + 25) //if user is over 25 mph the speed limit
+            {    
+                result = penalty + 60 + (fine * difference);
+                resultOutput.Text = "Your Speed is ILLEGAL. You have a fine of: $" + result.ToString(); 
+                resultOutput.ForeColor = Color.Red;
+            }
+            else if (speed > limit) //if user is over speed limit but under 25 
+            {
+                result = 60 + (fine * difference);
+                resultOutput.Text = difference.ToString();
+                resultOutput.Text = "Your Speed is illegal. You have no penalty, but a fine of: $" + result.ToString();
+                resultOutput.ForeColor = Color.Yellow;
+            }
+            else //if user is within the speed limit
+            {
+                resultOutput.Text = "Your speed is legal";
+                resultOutput.ForeColor = Color.Green;
+            }
+        }
+    }
+}
